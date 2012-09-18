199a7ef9721e899ee57f0c3d3485f3899222d795
Aaron Schrab <aaron@schrab.com>
2012-09-18 14:44:08 -0400

    Append to error arrays rather than setting them
    Make it less likely for errors to be overwritten by appending to error
    arrays instead of setting errors for a line, which assumed there were no
    previous errors for the line.

--- ruby_lsm/lsm.rb
+++ ruby_lsm/lsm.rb
@@ -46,7 +46,7 @@ class Entry #{{{
   attr_reader :lines, :errors
 
   def initialize #{{{
-    @errors = {}
+    @errors = Hash.new { |h,k| h[k] = [] }
   end #}}}
 
   def has_errors? #{{{
@@ -145,8 +145,7 @@ class Entry #{{{
 
         # Check for whitespace before the colon {{{
         if field[/\s$/]
-          @errors[current_line] = [
-            "No whitespace allowed between keyword and colon (sorry)" ]
+          @errors[current_line] << "No whitespace allowed between keyword and colon (sorry)"
           next
         end #}}}
 
@@ -163,17 +162,17 @@ class Entry #{{{
           begin
             send "#{meth}=", content
           rescue LSM::Error
-            @errors[current_line] = $!.explanation( field, content )
+            @errors[current_line] << $!.explanation( field, content )
           end
         else
-          @errors[current_line] = [ "Unknown keyword: #{field}" ]
+          @errors[current_line] << "Unknown keyword: #{field}"
         end #}}}
         #}}}
       when /^\s*$/ # Ignore empty lines before first field
       when nil; break
       else # Illegal line
-        @errors[current_line] = [ "No keyword found",
-          "  (lines beginning in column 1 must begin with a keyword)" ]
+        @errors[current_line] << "No keyword found"
+        @errors[current_line] << "  (lines beginning in column 1 must begin with a keyword)"
       end
     end
 
